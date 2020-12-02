a48d9331d3763393b12ece1494a530f07b27f564
Nerijus Viliusis <pcneriyus3@gmail.com>
2020-12-02 21:57:43 +0200

    week 2 : day 2

---
+++ scr/com/JavaCode/lessons/week2/enumTasks/Task.java
@@ -0,0 +1,10 @@
+package com.JavaCode.lessons.week2.enumTasks;
+
+public class Task {
+
+    public static void main(String[] args) {
+        DaysDemo daysMonday = new DaysDemo(Days.MONDAY);
+        daysMonday.whatDay();
+
+    }
+}
