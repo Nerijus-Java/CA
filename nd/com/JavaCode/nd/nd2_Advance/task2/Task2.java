fa5ffe98e3d28e65d835161de97f8c5df6e7e88d
Nerijus Viliusis <pcneriyus3@gmail.com>
2020-12-06 18:21:30 +0200

    Tank 2 Complete

--- nd/com/JavaCode/nd/nd2_Advance/task2/Task2.java
+++ nd/com/JavaCode/nd/nd2_Advance/task2/Task2.java
@@ -1,4 +1,17 @@
 package com.JavaCode.nd.nd2_Advance.task2;
 
+import java.util.Scanner;
+
 public class Task2 {
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        int[] numbers = new int[3];
+        for (int i = 0; i < 3; i++) {
+            System.out.print("Enter :");
+            numbers[i] = sc.nextInt();
+        }
+        System.out.println("The average is " + (numbers[0] + numbers[1] + numbers[2]) / 3);
+    }
 }
