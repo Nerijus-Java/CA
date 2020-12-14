7b634e796c388760324310d2cb81d611d45e59a2
Nerijus Viliusis <pcneriyus3@gmail.com>
2020-12-14 19:39:43 +0200

    task3

---
+++ scr/com/JavaCode/lessons/week4/day1/polmorfizmas/EvenNumber.java
@@ -0,0 +1,14 @@
+package com.JavaCode.lessons.week4.day1.polmorfizmas;
+
+import java.util.Random;
+
+public class EvenNumber extends Number{
+    Random random = new Random();
+
+    @Override
+    public void generate() {
+        for (int i = 0; i < intArray.length; i++) {
+            intArray[i] = random.nextInt(180) + 20;
+        }
+    }
+}
---
+++ scr/com/JavaCode/lessons/week4/day1/polmorfizmas/Task3.java
@@ -0,0 +1,8 @@
+package com.JavaCode.lessons.week4.day1.polmorfizmas;
+
+public class Task3 {
+
+    public static void main(String[] args) {
+
+    }
+}
