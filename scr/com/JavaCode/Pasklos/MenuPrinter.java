5de9791b862c2e988088e6d30a022271d61780ae
Nerijus Viliusis <pcneriyus3@gmail.com>
2020-12-22 16:57:09 +0200

    new branch

---
+++ scr/com/JavaCode/Pasklos/MenuPrinter.java
@@ -0,0 +1,6 @@
+package com.JavaCode.Pasklos;
+
+public class MenuPrinter {
+
+
+}
---
+++ scr/com/JavaCode/Pasklos/Program.java
@@ -0,0 +1,13 @@
+package com.JavaCode.Pasklos;
+
+public class Program {
+
+    public static void main(String[] args) {
+        InterestCalculator interestCalculator = new InterestCalculator(1000,10);
+
+        System.out.println(interestCalculator.toString());
+
+    }
+
+
+}
