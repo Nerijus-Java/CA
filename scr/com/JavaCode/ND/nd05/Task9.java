1fa9ad13733b8c70a3552f41d25285c92e84bf1b
Nerijus Viliusis <pcneriyus3@gmail.com>
2020-12-29 02:16:07 +0200

    task

--- scr/com/JavaCode/ND/nd05/Task9.java
+++ scr/com/JavaCode/ND/nd05/Task9.java
@@ -5,8 +5,31 @@ public class Task9 {
     //9. Write a Java program to insert an element (specific position) into an array.
 
     public static void main(String[] args) {
+        int[] arrayOne = new int[]{1, 2, 3, 4, 5, 6, 7, 8, 9};
 
+        int[] arrayTwo = addElement(arrayOne,2,22);
+        printArray(arrayTwo);
     }
 
+    public static int[] addElement(int[] array, int index, int element) {
+        int[] newArray = new int[array.length + 1];
+        int count = 0;
+        for (int i : array) {
+            if (count == index - 1) {
+                newArray[count] = element;
+                count++;
+            }
+            newArray[count] = i;
+            count++;
+        }
+        return newArray;
+    }
+
+    public static void printArray(int[] array){
+        for (int i : array){
+            System.out.print(i + "\t");
+        }
+        System.out.println();
+    }
 
 }
