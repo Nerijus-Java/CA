04279d11fc7eef698f86995ac4b0b0dd84714dec
Nerijus Viliusis <pcneriyus3@gmail.com>
2021-01-25 22:08:03 +0200

    week 10 day 1

--- scr/com/JavaCode/lessons/week1/Methods/task5/Task5.java
+++
@@ -1,9 +0,0 @@
-package com.JavaCode.lessons.week1.Methods.task5;
-
-
-public class Task5 {
-
-    public static void main(String[] args) {
-        System.out.println("me " + args[0] + " " + args[1]);
-    }
-}
